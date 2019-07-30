afa1b8825116dccf8a3e5b8b152724d30cf50cfa
Matt Williams <matt.seongjun@gmail.com>
2019-07-30 13:27:48 -0400

    Form done

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -2,8 +2,19 @@ import React from "react"
 
 function NewCardForm(props){
     return (
-        <form>
-            NewCardForm
+        <form className="ui form">
+            <div className="field">
+                <label>Title</label>
+                <input type="text" />
+            </div>
+            <div className="field">
+                <label>Content</label>
+                <textarea></textarea>
+            </div>
+            <div className="field">
+                <label>ImageUrl</label>
+                <input type="text" />
+            </div>
         </form>
     )
 }
