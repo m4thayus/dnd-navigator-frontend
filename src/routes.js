dc066d57340dbf147dca48ace797313b036e3881
Matt Williams <matt.seongjun@gmail.com>
2019-07-31 10:12:59 -0400

    More routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
-const URL = "http://localhost:3000/api/v1/"
+// const URL = "http://10.113.110.107:3000/api/v1/"
+// const URL = "http://localhost:3000/api/v1/"
+const URL = "http://10.113.109.245:3000/api/v1/"
 const CAMPAIGNS_URL = URL + "campaigns"
 const CHARACTERS_URL = URL + "characters"
 const PLAYERS_URL = URL + "players"
